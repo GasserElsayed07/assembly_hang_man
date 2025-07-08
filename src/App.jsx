e2e690d4b2363d3324879873128b6ed0fe4e8705
Gasser Mahmoud Ahmed Mahmoud ELSayed <gogo05tv@gmail.com>
2025-07-08 12:28:15 +0300

    Add a header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-
-import './App.css'
+import { useState } from "react";
+import Header from "./Header.jsx"
+import "./styles/App.css";
 
 export default function App() {
-
   return (
-    <main>
-      hi
-    </main>
-  )
+    <>
+      <Header />
+      <main>hi</main>
+    </>
+  );
 }
-
-
---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import "./styles/header.css"
+
+export default function Header(){
+    return (
+        <header>
+            <h2>Assembly: Endgame</h2>
+            <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+        </header>
+    )
+}
