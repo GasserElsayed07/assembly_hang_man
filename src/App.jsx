6a74e4200c15f222805c1a1d8b5cd393731d1045
Gasser Mahmoud Ahmed Mahmoud ELSayed <gogo05tv@gmail.com>
2025-07-08 16:46:19 +0300

    Add the hangman word, and the keybaord letters

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,76 @@ import { languages } from "./languages.js";
 import Language from "./language.jsx";
 
 export default function App() {
-  const [langs, setLangs] = useState([]);
-
+  const [langs, setLangs] = useState([])
+  const [letters, setLetters] = useState([])
+  const [currentWord, setCurrentWord] = useState("React")
+  console.log(currentWord.length)
+  
   useEffect(() => {
     setLangs(
       languages.map((lang, index) => (
         <Language
-          key={index}
-          name={lang.name}
-          background={lang.backgroundColor}
-          color={lang.color}
-          id={index}
+        key={index}
+        name={lang.name}
+        background={lang.backgroundColor}
+        color={lang.color}
+        id={index}
         />
       ))
     );
-  }, []);
+    
+    setLetters(
+      currentWord.split("").map((letter, index) => {
+        const styles = {
+          width : "40px",
+          height : "40px",
+          borderBottom: "1px solid #F9F4DA",
+          background: "#323232",
+          margin: "2px",
+          
+          fontWeight: "700",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          color: "#F9F4DA",
+          fontSize: "18px",
+        }
+        return(
+          <span style={styles}>{letter.toUpperCase()}</span>
+        )
+      })
+    )
+  }, [])
+
+  let asci = 65;
+  let alphabet = [];
+  for (let i = 0; i < 26; i++){
+    const currentLetter = String.fromCharCode(asci);
+    alphabet.push(currentLetter);
+    asci++;
+  }
+  
+  const keyboard = alphabet.map((letter, index) => {
+    const styles = {
+      backgroundColor: "#FCBA29",
+      width: "40px",
+      height: "40px",
+      borderRadius: "4px",
+      border: "1px solid #D7D7D7",
+      fontWeight: "600",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: "16px",
+      color: "#1E1E1E",
+      margin: "4px",
+    }
+
+    return (
+      <button style={styles}>{letter}</button>
+    )
+  })
+
   return (
     <>
       <Header />
@@ -28,6 +83,8 @@ export default function App() {
         <p>Well done! 🎉</p>
       </div>
       <section className="languages">{langs}</section>
+      <section className="letters">{letters}</section>
+      <section className="keyboard">{keyboard}</section>
       <main>hi</main>
     </>
   );
